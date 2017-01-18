b30bc50243dbc32d8d9b53858a9afa3afc31b8ec
rylr <league@WTS9.attlocal.net>
2017-01-17 17:06:31 -0800

    day 2 workshop

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -6,10 +6,12 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
-		// START HERE
+		Robot cookie = new Robot ();
+		cookie.penDown();
+for(int i=0;i<4;i++){
+cookie.turn(90);
+cookie.move(150);
+}
 
-	
-		
-		
 	}
 }
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -6,22 +6,28 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+	Robot cookie = new Robot ();
+		cookie.penDown();
 		// 5. Set your robot's pen to the down position
 
 		// 3. Set the robot to go at max speed (10)
-
+cookie.setSpeed(10);
 		// 4. Do the following (steps 6-9) 75 times
-
+for (int i = 0; i < 75; i++) {
 			// 7. Change the pen color to random
-	
+	cookie.setRandomPenColor();
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-	
+cookie.move(5*i);
+
+
+
 			// 2. Turn the robot 1/3 of 360 degrees to the right
-	
+		cookie.turn(360/3);
+
 			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
 	
 			// 9. Set the pen width to i
+	}
 
 	}
 }

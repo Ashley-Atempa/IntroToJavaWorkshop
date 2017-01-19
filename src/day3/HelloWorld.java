0d3e2158534d66c7d91a4670d1fc81e461fd18b0
Ashley-Atempa <league@WorthyMini.attlocal.net>
2017-01-18 16:59:35 -0800

    18 jan

---
+++ src/day3/HelloWorld.java
@@ -0,0 +1,14 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+import sun.applet.Main;
+
+public class HelloWorld {
+public static void main(String[] args) {
+	System.out.println("Ashley is Bad");
+JOptionPane.showMessageDialog(null, "hey hey whats up with ur computer");
+String Ashley=JOptionPane.showInputDialog(null,"hey enter your name ");
+JOptionPane.showMessageDialog(null, Ashley+" is a cool person");
+}
+}
--- src/day3/MadlibsOfTheAmazon.java
+++ src/day3/MadlibsOfTheAmazon.java
@@ -9,21 +9,25 @@ public class MadlibsOfTheAmazon {
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
 
 		// Get the user to enter an adjective
-
+		String adj=JOptionPane.showInputDialog(null,"hey enter your adjective ");
 		// Get the user to enter a type of liquid
-
+String liquid=JOptionPane.showInputDialog(null, "enter your favorite drink");
 		// Get the user to enter a body part
-
+String part=JOptionPane.showInputDialog(null,"enter a body part");
 		// Get the user to enter a verb
-
+String Verb=JOptionPane.showInputDialog(null,"enter a verb");
 		// Get the user to enter a place
-
+String Place=JOptionPane.showInputDialog(null," enter ur place to go");
 		// Fit the user's words into this sentence, and save it in a String:
+JOptionPane.showMessageDialog(null, "i love to eat fruits "+adj+" eat.");
 		// Piranhas are more [adjective] during the day, so cross the river at
-		// night. Piranhas are attracted to fresh [type of liquid] and will most
+	// night. Piranhas are attracted to fresh [type of liquid] and will most
+JOptionPane.showMessageDialog(null," i like to eat "+ liquid + "eating,");
 		// likely take a bite out of your [body part] if you [verb]. Whatever
+JOptionPane.showMessageDialog(null,"i dont like to eat "+part+" hand,");
 		// you do, if you have an open wound, try to find another way to get
-		// back to the [place]. Good luck!
+// back to the [place]. Good luck!
+JOptionPane.showMessageDialog(null,"i woould like to travel "+Place+" Chicago");
 		
 		// Make a pop-up for the final story. You can use \n to go to the next line.
 		

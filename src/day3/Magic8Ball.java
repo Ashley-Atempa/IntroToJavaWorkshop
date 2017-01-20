6576e4ad83a2d56f24faced40326132996fe58c2
Savera <Level 2>
2017-01-19 16:56:35 -0800

    Magic 8 Ball

---
+++ src/day3/Magic8Ball.java
@@ -0,0 +1,41 @@
+package day3;
+
+//Copyright Wintriss Technical Schools 2013
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Magic8Ball {
+
+	// 1. Make a main method that includes all the steps below….
+	public static void main(String[] args) {
+	
+
+	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+int cookie=new Random().nextInt(4);
+	// 3. Print out this variable
+System.out.println(cookie);
+	// 4. Get the user to enter a question for the 8 ball
+String question =JOptionPane.showInputDialog(null,"please enter a a question for 8 ball");
+	// 5. If the random number is 0
+if (cookie==0){
+	// -- tell the user "Yes"
+JOptionPane.showMessageDialog(null,"yes");}
+	// 6. If the random number is 1
+if (cookie==1){
+	// -- tell the user "No"
+	JOptionPane.showMessageDialog(null,"no");}
+	// 7. If the random number is 2
+if (cookie==2){
+	// -- tell the user "Maybe you should ask Google?"
+	JOptionPane.showMessageDialog(null,"maybe you should ask google");}
+	// 8. If the random number is 3
+if (cookie==3){
+	// -- write your own answer
+	JOptionPane.showMessageDialog(null,"it feels nice to be alive");}
+}
+}
+
+
+
+
